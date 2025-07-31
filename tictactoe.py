a11b8289360c350370ef63fe6bb15b1097355d31
FfionLee <120313877+FfionLee@users.noreply.github.com>
2025-07-31 16:25:00 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 ui=Tk()
 ui.geometry('450x480')
@@ -7,6 +8,25 @@ ui.config(bg='sienna')
 buttons=[
 
 ]
+
+def winner(b,currentplayer):
+    for row in range(3):
+        if b[row][0]==b[row][1]==b[row][2]==currentplayer:
+            return True
+    for column in range(3):
+        if b[0][column]==b[1][column]==b[2][column]==currentplayer:
+            return True
+    if b[0][0]==b[1][1]==b[2][2]==currentplayer:
+        return True
+    if b[2][0]==b[1][1]==b[0][2]==currentplayer:
+        return True
+
+def checkfull(b):
+    for i in range(3):
+        for j in range(3):
+            if b[i][j]==' ':
+                return False
+    return True
 
 def game():
     b=[[' ' for _ in range(3)] for _ in range(3)]
@@ -19,6 +39,16 @@ def game():
             button=buttons[row][column]
             button['text']=currentplayer
             button['state']='disabled'
+            if winner(b,currentplayer)==True:
+                messagebox.showinfo('winner','The winner is '+currentplayer)
+            elif checkfull(b):
+                messagebox.showinfo('tie','The game is a tie')
+
+            else:
+                if currentplayer=='x':
+                    currentplayer='o'
+                else:
+                    currentplayer='x'
     for i in range(3):
         row=[]
         for j in range(3):
